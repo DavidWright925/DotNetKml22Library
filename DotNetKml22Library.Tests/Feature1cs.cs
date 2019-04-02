3db19255aeeb4094f2b51002a6f56e61821637ff
david.wright <david.wright@equest.com>
2019-04-02 14:49:59 -0700

    New Feature1 Added

---
+++ DotNetKml22Library.Tests/Feature1cs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetKml22Library.Tests
+{
+    /// <summary>
+    /// This is a test class while I figure out GIT.
+    /// </summary>
+    class Feature1cs
+    {
+    }
+}
